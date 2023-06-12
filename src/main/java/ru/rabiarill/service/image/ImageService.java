503cf041d7b67b18db58d912f92d979badb0e833
Vladimir Dorohov <doomreanger@mail.ru>
2023-06-12 13:24:27 +0300

    add image service

---
+++ src/main/java/ru/rabiarill/service/image/ImageService.java
@@ -0,0 +1,23 @@
+package ru.rabiarill.service.image;
+
+import ru.rabiarill.model.Image;
+
+import java.util.List;
+
+public interface ImageService {
+
+   /**
+    * Method save images in database.
+    *
+    * @param images
+    * @return <code>List<Image></code> object
+    */
+   List<Image> save(List<Image> images);
+
+   /**
+    * Method delete images in database.
+    *
+    * @param images
+    */
+   void delete(List<Image> images);
+}
---
+++ src/main/java/ru/rabiarill/service/image/impl/ImageServiceImpl.java
@@ -0,0 +1,40 @@
+package ru.rabiarill.service.image.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.rabiarill.model.Image;
+import ru.rabiarill.repository.ImageRepository;
+import ru.rabiarill.service.image.ImageService;
+
+import java.util.List;
+
+@Service("imageService")
+@Transactional(readOnly = true)
+public class ImageServiceImpl implements ImageService {
+
+   private final ImageRepository imageRepository;
+
+   @Autowired
+   public ImageServiceImpl(ImageRepository imageRepository) {
+      this.imageRepository = imageRepository;
+   }
+
+   /**
+    * @see ImageService#save(List)
+    */
+   @Override
+   @Transactional
+   public List<Image> save(List<Image> images) {
+      return imageRepository.saveAll(images);
+   }
+
+   /**
+    * @see ImageService#delete(List)
+    */
+   @Override
+   @Transactional
+   public void delete(List<Image> images) {
+      imageRepository.deleteAll(images);
+   }
+}
